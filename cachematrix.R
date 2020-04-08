c370da4dd18a9ca04ca34e15e97cc0402b40ea20
AAbdelsameia <aabdelsameia@aucegypt.edu>
2020-04-08 05:39:19 +0200

    Assignment2Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the function makeCacheMatrix is used to create a speical matrix that can store its inverse in msol.
+## the special matrix is then called by a functoin cacheSolve that checks if the matrix's inverse is already
+##calculated and stored in msol. If so, it returns the already saved solution. If not, it calcualtes the inverse, 
+## updates the solution in the special msol matrix, and returns the solution. 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+
+## This functoin creates a special matrix that can store its inverse in msol
+
+makeCacheMatrix <- function(m = matrix()) {
+  msol <- NULL
+  set <- function(y) {
+    m <<- y
+    msol <<- NULL
+  }
+  get <- function() m
+  setmean <- function(sol) msol <<- sol
+  getmean <- function() msol
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## This functoin is called with speical matrix as input to check for previos saved inverses,
+## if there is non, it calcuates the new inverse, updates msol and prints the solutoin.  
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(m, ...) {
+
+  msol <- m$getmean()
+  if(!is.null(msol)) {
+    message("getting cached data")
+    return(msol)
+  }
+  data <- m$get()
+  msol <- solve(data, ...)
+  m$setmean(msol)
+  msol
         ## Return a matrix that is the inverse of 'x'
 }
